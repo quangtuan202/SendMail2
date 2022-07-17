8ced0625ca8a1623291c108a6d1866ad6214af3f
tuan <quangtuan202@gmail.com>
2022-07-17 09:14:21 +0700

    Initial commit

---
+++ app/src/main/java/com/example/sendmail2/GmailAPI.java
@@ -0,0 +1,163 @@
+package com.example.sendmail2;
+
+import android.content.Context;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.security.GeneralSecurityException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.json.JSONObject;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.client.util.Base64;
+import com.google.api.client.util.StringUtils;
+import com.google.api.services.gmail.Gmail;
+import com.google.api.services.gmail.model.ListMessagesResponse;
+import com.google.api.services.gmail.model.Message;
+
+public class GmailAPI {
+
+	
+	/*
+	1.Get code : 
+https://accounts.google.com/o/oauth2/v2/auth?
+ scope=https://mail.google.com&
+ access_type=offline&
+ redirect_uri=http://localhost&
+ response_type=code&
+ client_id=[Client ID]
+
+2. Get access_token and refresh_token
+ curl \
+--request POST \
+--data "code=[Authentcation code from authorization link]&client_id=[Application Client Id]&client_secret=[Application Client Secret]&redirect_uri=http://localhost&grant_type=authorization_code" \
+https://accounts.google.com/o/oauth2/token
+
+3.Get new access_token using refresh_token
+curl \
+--request POST \
+--data "client_id=[your_client_id]&client_secret=[your_client_secret]&refresh_token=[refresh_token]&grant_type=refresh_token" \
+https://accounts.google.com/o/oauth2/token
+	
+*/
+	private static final String APPLICATION_NAME = "Gmail API Java Quickstart";
+	private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
+	private static final String user = "me";
+	static Gmail service = null;
+	private static File filePath = new File("/raw/credentials.json");
+
+	private static String REFRESH_TOKEN="1//04DBM8TCw9uEWCgYIARAAGAQSNwF-L9IrONRZbIl3mMedO2h4a51CfWE0f6QS0ghRunr8JB1ayi95seY3vniDIr4sMinJJHBG_zE";
+
+//	public static void main(String[] args) throws IOException, GeneralSecurityException {
+//
+//		getGmailService();
+//
+//		getMailBody("Google");
+//
+//	}
+
+	public static void getMailBody(String searchString) throws IOException {
+
+		// Access Gmail inbox
+
+		Gmail.Users.Messages.List request = service.users().messages().list(user).setQ(searchString);
+
+		ListMessagesResponse messagesResponse = request.execute();
+		request.setPageToken(messagesResponse.getNextPageToken());
+
+		// Get ID of the email you are looking for
+		String messageId = messagesResponse.getMessages().get(0).getId();
+
+		Message message = service.users().messages().get(user, messageId).execute();
+
+		// Print email body
+
+		String emailBody = StringUtils
+				.newStringUtf8(Base64.decodeBase64(message.getPayload().getParts().get(0).getBody().getData()));
+
+		System.out.println("Email body : " + emailBody);
+
+	}
+
+	public static Gmail getGmailService(Context context) throws IOException, GeneralSecurityException {
+
+//		InputStream in = new FileInputStream(filePath); // Read credentials.json
+
+		InputStream in = context.getResources().openRawResource(R.raw.credentials);
+		GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+		// Credential builder
+
+		Credential authorize = new GoogleCredential.Builder().setTransport(GoogleNetHttpTransport.newTrustedTransport())
+				.setJsonFactory(JSON_FACTORY)
+				.setClientSecrets(clientSecrets.getDetails().getClientId().toString(),
+						clientSecrets.getDetails().getClientSecret().toString())
+				.build().setAccessToken(getAccessToken()).setRefreshToken(REFRESH_TOKEN);//Replace this
+
+		// Create Gmail service
+		final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+		service = new Gmail.Builder(HTTP_TRANSPORT, JSON_FACTORY, authorize)
+				.setApplicationName(GmailAPI.APPLICATION_NAME).build();
+
+		return service;
+	}
+
+	private static String getAccessToken() {
+
+		try {
+			Map<String, Object> params = new LinkedHashMap<>();
+			params.put("grant_type", "refresh_token");
+			params.put("client_id", "337247275082-qu0d26nci2lkol6bd66ccm3846vmviio.apps.googleusercontent.com"); //Replace this
+			params.put("client_secret", "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"); //Replace this
+			params.put("refresh_token", REFRESH_TOKEN); //Replace this
+
+			StringBuilder postData = new StringBuilder();
+			for (Map.Entry<String, Object> param : params.entrySet()) {
+				if (postData.length() != 0) {
+					postData.append('&');
+				}
+				postData.append(URLEncoder.encode(param.getKey(), "UTF-8"));
+				postData.append('=');
+				postData.append(URLEncoder.encode(String.valueOf(param.getValue()), "UTF-8"));
+			}
+			byte[] postDataBytes = postData.toString().getBytes("UTF-8");
+
+			URL url = new URL("https://accounts.google.com/o/oauth2/token");
+			HttpURLConnection con = (HttpURLConnection) url.openConnection();
+			con.setDoOutput(true);
+			con.setUseCaches(false);
+			con.setRequestMethod("POST");
+			con.getOutputStream().write(postDataBytes);
+
+			BufferedReader reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			StringBuffer buffer = new StringBuffer();
+			for (String line = reader.readLine(); line != null; line = reader.readLine()) {
+				buffer.append(line);
+			}
+
+			JSONObject json = new JSONObject(buffer.toString());
+			String accessToken = json.getString("access_token");
+			return accessToken;
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		return null;
+	}
+
+}
---
+++ app/src/main/java/com/example/sendmail2/MainActivity.java
@@ -0,0 +1,94 @@
+package com.example.sendmail2;
+
+import static com.example.sendmail2.GmailOperations.createEmail;
+import static com.example.sendmail2.GmailOperations.createMessageWithEmail;
+import static com.example.sendmail2.GmailOperations.sendEmail;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.api.client.util.ExponentialBackOff;
+import com.google.api.services.gmail.Gmail;
+import com.google.api.services.gmail.GmailScopes;
+import com.google.api.services.gmail.model.Message;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Arrays;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button=findViewById(R.id.button);
+        button.setOnClickListener(click-> {Toast.makeText(this,"Clicked",Toast.LENGTH_SHORT).show();
+            try {
+                sendEmail();
+            } catch (MessagingException e) {
+                e.printStackTrace();
+            } catch (GeneralSecurityException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    public void sendEmail() throws IOException, GeneralSecurityException, MessagingException {
+        // Must run on another thread
+        Runnable runnable= () -> {
+            Gmail service = null;
+            try {
+                service = GmailAPI.getGmailService(this);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (GeneralSecurityException e) {
+                e.printStackTrace();
+            }
+            MimeMessage Mimemessage = null;
+            try {
+                Mimemessage = createEmail("quangtuan202@gmail.com","quangtuan202@gmail.com","This my demo test subject","This is my body text");
+            } catch (MessagingException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Message message = null;
+            try {
+                message = createMessageWithEmail(Mimemessage);
+            } catch (MessagingException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                message = service.users().messages().send("me", message).execute();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("Message id: " + message.getId());
+            try {
+                System.out.println(message.toPrettyString());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        };
+        new Thread(runnable).start();
+
+
+    }
+
+}
